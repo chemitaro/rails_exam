b7750228bad3064c0b50bb42c7cafdefcdb7e02d
“iwasawa <iwasi_44@hotmail.com>
2021-09-14 19:35:10 +0900

    投稿確認機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,43 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "つぶやきました"
+    else
+      render :new
+    end
+  end
+  def confirm
+    @post = Post.new(post_params)
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "編集しました"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "削除しました"
+
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
